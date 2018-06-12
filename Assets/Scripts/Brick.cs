0b123e11e9a215b628c3bc4696c8729cb2471f4f
HBock <42hugobock@gmail.com>
2018-06-12 23:49:50 +0200

    First brick implementation

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(MeshCollider))]
+public class Brick : MonoBehaviour, IDamagable, IDestroyable {
+
+	#region Constants
+
+	private const int InitialHp = 2;
+
+	#endregion
+
+	#region Events
+
+	private UnityEvent _noHpRemaining;
+
+	#endregion
+
+	#region Fields
+
+	private int _hp;
+
+	#endregion
+
+	#region Properties
+
+
+	public int Hp
+	{
+		get { return _hp; }
+		set
+		{
+			if (value >= 0) _hp = value;
+			else
+			{
+				_hp = 0;
+				_noHpRemaining.Invoke();
+			}
+		}
+	}
+
+	#endregion
+
+	#region MonoBehaviour
+
+	private void Start()
+	{
+		Initialize();
+	}
+
+	#endregion
+
+	#region Methods
+
+	private void Initialize()
+	{
+		Hp = InitialHp;
+		_noHpRemaining = new UnityEvent();
+		_noHpRemaining.AddListener(NoHpRemainingCallback);
+	}
+
+	public void Destroy()
+	{
+		Destroy(this.gameObject);
+	}
+
+	public void TakeDamage(int amount)
+	{
+		Hp -= amount;
+	}
+
+	/// <summary>
+	/// Action to take when the brick hits zero hp
+	/// </summary>
+	private void NoHpRemainingCallback()
+	{
+		Destroy();
+	}
+
+	#endregion
+
+	#region Messages
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Ball"))
+		{
+			TakeDamage(1);
+		}
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/IDamagable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagable
+{
+	#region Properties
+
+	int Hp { get; set; }
+
+	#endregion
+
+
+	#region Methods
+
+	void TakeDamage(int amount);
+
+	#endregion
+
+}
