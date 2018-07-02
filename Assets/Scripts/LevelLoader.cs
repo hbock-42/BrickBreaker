18cf7a967f9a24090374d2c7c0364555abf32ec9
HBock <42hugobock@gmail.com>
2018-07-02 23:04:04 +0200

    SetupStrings (strings manager)

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+public static class LevelLoader
+{
+	public static void FromXml(string path)
+	{
+		//var xDoc = XDocument.Load();
+		
+	}
+}
---
+++ Assets/Scripts/SetupStrings.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;
+using UnityEngine;
+
+public static class SetupStrings
+{
+	public static string LevelSavePath = Path.Combine(Application.dataPath, "LevelsData");
+}
