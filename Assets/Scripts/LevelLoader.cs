2a0fb5e101545b4bcefbe76c3cf8b89e3ca780b3
HBock <42hugobock@gmail.com>
2018-07-02 23:10:00 +0200

    Working on level loader - should make a project common to the game and the levelBuilder to avoid class duplication

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
@@ -8,7 +9,9 @@ public static class LevelLoader
 {
 	public static void FromXml(string path)
 	{
-		//var xDoc = XDocument.Load();
-		
+		var xDoc = XDocument.Load(Path.Combine(SetupStrings.LevelSavePath, "LevelTest.xml"));
+
 	}
+
+	// Make a common repo to not duplicate class
 }
